1a49ebcdc1d0bf332357d7ce6698c7b4084a0757
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-30 11:54:17 -0400

    create basic trafficLight

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
 export const TrafficLight = () => {
+
+    const trafficLightStyle = {
+        alignItems: "center",
+        background: "black",
+        borderRadius: "10px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "auto",
+        padding: "0 0 5px 0",
+        width: "70px",
+    }
+    const lightStyle = {
+        borderRadius: "50%",
+        width: "60px",
+        height: "60px",
+        background: "white",
+        margin: "5px 5px 0",
+    }
+
     return (
-        <div>
-            
+        <div style={ trafficLightStyle }>
+            <div style={ lightStyle }></div>
+            <div style={ lightStyle }></div>
+            <div style={ lightStyle }></div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import ReactDOM from 'react-dom';
 import { TrafficLight } from './components/TrafficLight';
 
 function App() {
+
+    const myStyle = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+
     return (
-        <>
+        <div style={ myStyle }>
             <TrafficLight />
-        </>
+        </div>
     )
 }
 
